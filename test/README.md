88b2ceae85879b0d69206ce8edc406e8a92ac358
Paco Martín Fernández <fmartinfdez@gmail.com>
2017-03-06 14:17:19 +0000

    Add some tests

---
+++ test/README.md
@@ -0,0 +1,13 @@
+# Python API Client IBM Quantum Experience TEST
+
+To run the tests (unittest), you need configure the **API_TOKEN** of the Quantum Experience Platform in *config.py* file:
+
+```
+API_TOKEN = 'YOUR_API_TOKEN'
+```
+
+Then, you have to run:
+
+```
+python test_IBMQuantumExperience.py
+```
---
+++ test/test_IBMQuantumExperience.py
@@ -0,0 +1,61 @@
+from IBMQuantumExperience import IBMQuantumExperience
+import unittest
+from config import *
+
+
+class TestQX(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    """ ---------------------------------
+            TESTS
+        ---------------------------------
+    """
+
+    def test_api_auth_token(self):
+        '''
+        Authentication with Quantum Experience Platform
+        '''
+        api = IBMQuantumExperience(API_TOKEN)
+        credential = api._checkCredentials()
+        self.assertTrue(credential)
+
+    def test_api_last_codes(self):
+        '''
+        Check last code by user authenticated
+        '''
+        api = IBMQuantumExperience(API_TOKEN)
+        self.assertIsNotNone(api.getLastCodes())
+
+    def test_api_run_experiment(self):
+        '''
+        Check run an experiment by user authenticated
+        '''
+        api = IBMQuantumExperience(API_TOKEN)
+        qasm = "IBMQASM 2.0;\n\ninclude \"qelib1.inc\";\nqreg q[5];\ncreg c[5];\nu2(-4*pi/3,2*pi) q[0];\nu2(-3*pi/2,2*pi) q[0];\nu3(-pi,0,-pi) q[0];\nu3(-pi,0,-pi/2) q[0];\nu2(pi,-pi/2) q[0];\nu3(-pi,0,-pi/2) q[0];\nmeasure q -> c;\n"
+        device = 'simulator'
+        shots = 1
+        experiment = api.runExperiment(qasm, device, shots)
+        self.assertIsNotNone(experiment['status'])
+
+    def test_api_run_job(self):
+        '''
+        Check run an job by user authenticated
+        '''
+        api = IBMQuantumExperience(API_TOKEN)
+        qasm1 = { "qasm": "IBMQASM 2.0;\n\ninclude \"qelib1.inc\";\nqreg q[5];\ncreg c[5];\nu2(-4*pi/3,2*pi) q[0];\nu2(-3*pi/2,2*pi) q[0];\nu3(-pi,0,-pi) q[0];\nu3(-pi,0,-pi/2) q[0];\nu2(pi,-pi/2) q[0];\nu3(-pi,0,-pi/2) q[0];\nmeasure q -> c;\n"}
+        qasm2 = { "qasm": "IBMQASM 2.0;\n\ninclude \"qelib1.inc\";\nqreg q[5];\ncreg c[5];\nx q[0];\nmeasure q -> c;\n"}
+        qasms = [qasm1, qasm2]
+        device = 'simulator'
+        shots = 1
+        job = api.runJob(qasms, device, shots)
+        self.assertIsNotNone(job['status'])
+
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestQX)
+    unittest.TextTestRunner(verbosity=2).run(suite)
