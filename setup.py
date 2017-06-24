43da85c4244c446818e155a9a82f07b17534c883
cclauss <cclauss@bluewin.ch>
2017-06-24 17:29:07 +0200

    flake8 recommended changes

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
-setup(name = 'IBMQuantumExperience',
-      packages = ['IBMQuantumExperience'], # this must be the same as the name above
+setup(name='IBMQuantumExperience',
+      packages=['IBMQuantumExperience'],  # this must be the same as the name above
       version='1.5',
       author='IBM Research ETX',
       description='A Python library for the Quantum Experience API.',
       author_email='pacom@us.ibm.com',
-      url = 'https://github.com/IBM/qiskit-api-py',
-      keywords = ['ibm', 'quantum computer', 'quantum experience'],
+      url='https://github.com/IBM/qiskit-api-py',
+      keywords=['ibm', 'quantum computer', 'quantum experience'],
       license='Apache-2.0',
       install_requires=[
-        'requests'
+          'requests'
       ],
       zip_safe=False)
