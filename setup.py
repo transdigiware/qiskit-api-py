ff749eb772c0ec494576049d1ab814ea7afd1de6
Paco Martín Fernández <fmartinfdez@gmail.com>
2017-07-25 09:53:12 +0100

    Version 1.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='IBMQuantumExperience',
       packages=['IBMQuantumExperience'],  # this must be the same as the name above
-      version='1.7',
+      version='1.8',
       author='IBM Research ETX',
       description='A Python library for the Quantum Experience API.',
       author_email='pacom@us.ibm.com',
