41492f71676258d8dcbf46bad0f84592d4c9f5af
Diego M. Rodriguez <diego.plan9@gmail.com>
2017-10-30 17:45:06 +0100

    Add package __version__ attribute
    Define `__version__` as per PEP 396. Please note the version is still
    also defined at setup.py (and both values need to be updated upon new
    releases).

--- IBMQuantumExperience/__init__.py
+++ IBMQuantumExperience/__init__.py
@@ -3,3 +3,5 @@ from .IBMQuantumExperience import ApiError
 from .IBMQuantumExperience import BadBackendError
 from .IBMQuantumExperience import CredentialsError
 from .IBMQuantumExperience import RegisterSizeError
+
+__version__ = '1.8.13'  # this should match setup.py:version parameter
