e7c145b4630c6484a59a9484bbec2c1387e59af6
Paco Martín Fernández <fmartinfdez@gmail.com>
2018-07-05 13:32:18 +0100

    version 1.9.7

--- IBMQuantumExperience/__init__.py
+++ IBMQuantumExperience/__init__.py
@@ -4,4 +4,4 @@ from .IBMQuantumExperience import BadBackendError
 from .IBMQuantumExperience import CredentialsError
 from .IBMQuantumExperience import RegisterSizeError
 
-__version__ = '1.9.6'  # this should match setup.py:version parameter
+__version__ = '1.9.7'  # this should match setup.py:version parameter
